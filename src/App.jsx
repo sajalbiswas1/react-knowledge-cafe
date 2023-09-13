b291075de4b87583aa884b3ba202de1e9b015aee
Programing Hero <sajalb1899@gmail.com>
2023-09-13 21:59:59 +0600

    blogs some feture added

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,5 @@
 import './App.css'
+import Blogs from './component/Blogs/Blogs'
 import Header from './component/Header/Header'
 
 function App() {
@@ -6,6 +7,7 @@ function App() {
   return (
     <>
       <Header></Header>
+      <Blogs></Blogs>
       
     </>
   )
--- src/component/Blogs/Blogs.jsx
+++ src/component/Blogs/Blogs.jsx
@@ -1,4 +1,14 @@
+import { useEffect } from "react";
+import { useState } from "react";
+
 const Blogs = () => {
+    const [blogs, setBlogs]= useState([])
+
+    useEffect(()=>{
+        fetch('blog.json')
+        .then(res => res.json())
+        .then(data => setBlogs(data))
+    },[])
     return (
         <div>
             
